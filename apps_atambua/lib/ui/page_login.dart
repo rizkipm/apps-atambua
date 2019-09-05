31727ccd207a3f4c6d725a2df6c192ed7d7b277d
Rizki Syaputra <rizki@udacoding.com>
2019-09-05 15:22:58 +0800

    update page login

--- apps_atambua/lib/ui/page_login.dart
+++ apps_atambua/lib/ui/page_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'page_register.dart';
 
 class PageLogin extends StatefulWidget {
   @override
@@ -10,13 +11,13 @@ class _PageLoginState extends State<PageLogin> {
   TextEditingController etUsername = new TextEditingController();
   TextEditingController etPassword = new TextEditingController();
 
-  String sex = "";
-
-  void _pilihSex(String value){
-    setState(() {
-      sex = value;
-    });
-  }
+//  String sex = "";
+//
+//  void _pilihSex(String value){
+//    setState(() {
+//      sex = value;
+//    });
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +26,9 @@ class _PageLoginState extends State<PageLogin> {
         padding: EdgeInsets.only(top: 35.0),
         child: Column(
           children: <Widget>[
-            Text('Register Form'),
+            Text('Login Form', style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold,
+              color: Colors.blueGrey
+            ),),
             SizedBox(height: 20.0,),
             Image.asset('assets/bea_cukai.png', height: 100.0, width: 100.0,),
             Padding(
@@ -40,7 +43,45 @@ class _PageLoginState extends State<PageLogin> {
                   )
                 ),
               ),
-            )
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                controller: etPassword,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                    )
+                ),
+              ),
+            ),
+
+
+            Padding(
+              padding: EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 0),
+              child: MaterialButton(
+                color: Colors.blueGrey,
+                textColor: Colors.white,
+                child: Text('Login'),
+                onPressed: (){},
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0),
+              child: MaterialButton(
+                textColor: Colors.blueGrey,
+                child: Text('Belum Punya Akun? Silahkan Register'),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>PageRegister()));
+                },
+              ),
+            ),
           ],
         ),
       ),
